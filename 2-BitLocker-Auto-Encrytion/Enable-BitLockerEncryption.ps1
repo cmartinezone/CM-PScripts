6c71bf98798bacec79687284fbf5e43db7bfdca3
Carlos Martinez <carlos0492@outlook.com>
2019-07-03 09:53:52 -0400

    Update Enable-BitLockerEncryption.ps1

--- 2-BitLocker-Auto-Encrytion/Enable-BitLockerEncryption.ps1
+++ 2-BitLocker-Auto-Encrytion/Enable-BitLockerEncryption.ps1
@@ -1,33 +1,48 @@
 #Author: Carlos Martinez  - GitHub @cmartinezone
-#Date: 7-1-2019 
+#Date: 7-3-2019 
  
 #Get-TPM Status
 $TPM = Get-Tpm | Select-Object -Property TpmPresent, AutoProvisioning -ErrorAction SilentlyContinue
 
 #Get-Drive encryption status
-$DriveStatus = Get-BitLockerVolume -MountPoint "C:\" | Select-Object -Property VolumeStatus
+$DriveStatus = Get-BitLockerVolume -MountPoint "C:" | Select-Object -Property VolumeStatus
  
 #If Drive is fully decrypted and TPM is anable and TPM is actived
-if (($DriveStatus.VolumeStatus -eq "FullyDecrypted") -and  ($TPM.TpmPresent -eq "True") -and ($TPM.AutoProvisioning -eq "Enabled")) {
+if (($DriveStatus.VolumeStatus -eq "FullyDecrypted") -and ($TPM.TpmPresent -eq "True") -and ($TPM.AutoProvisioning -eq "Enabled")) {
 
     #Extract Pin number from computername if 6 digits number is included on the name
     $PinNumber = $env:COMPUTERNAME -replace "[^0-9]" , ''
     
-    #Example Default Pin number set if the computer name doesn't contain numbers:
-    #$PinNumber = "123456"
-    
-    #Create security pin
-    $SecureString = ConvertTo-SecureString $PinNumber -AsPlainText -Force
-
+    #Verify if the number extracted from the computer name meet the requirements of 8 digits.
+    if ($PinNumber.Length -eq 8) 
+    {
+        #Create security pin
+        $SecureString = ConvertTo-SecureString $PinNumber -AsPlainText -Force
+    }
+    else 
+    {
+        #Set default Pin number if the computer name doesn't contain 8 degit numbers:
+        $PinNumber = "12345678"
+        $SecureString = ConvertTo-SecureString $PinNumber -AsPlainText -Force
+    }
+   
     #Add RecoveryKey to the drive requires by GPO
-    Add-BitLockerKeyProtector -MountPoint "C:" -RecoveryPasswordProtector
+    Add-BitLockerKeyProtector -MountPoint "C:" -RecoveryPasswordProtector -ErrorAction SilentlyContinue
 
-    #Enable Encryption
-    Enable-BitLocker -MountPoint "C:\"  -EncryptionMethod XtsAes256  -TpmAndPinProtector -Pin $SecureString -SkipHardwareTest
+    #Enable BitLocker Encryption
+    Enable-BitLocker -MountPoint "C:"  -EncryptionMethod XtsAes256  -TpmAndPinProtector -Pin $SecureString -SkipHardwareTest -ErrorAction SilentlyContinue
 
-    #Push Recovery Key to AD:
-    $Drive = Get-BitLockerVolume -MountPoint "C:\"
-    Backup-BitLockerKeyProtector -MountPoint "C:\" -KeyProtectorId $Drive.KeyProtector[0].KeyProtectorId
+    #Backup Recovery Keys Asociated with the C: Drive to AD:
+    $Drive = Get-BitLockerVolume -MountPoint "C:"
+    
+    foreach ($KeyProtector in $Drive.KeyProtector) {
+        
+        if ($KeyProtector.KeyProtectorType -eq "RecoveryPassword") {
+        
+            Backup-BitLockerKeyProtector -MountPoint "C:" -KeyProtectorId $KeyProtector.KeyProtectorId 
+
+        }
+    }
 
 }
 
